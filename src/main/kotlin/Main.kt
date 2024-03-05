bab7ebd7093492ac74c1facc248a3b82811c370a
rmungar <mgraul@iesrafaelalberti.es>
2024-03-05 18:21:29 +0100

    Loot de Trials.txt listo, añadir los otros y continuar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 package org.practicatrim2
 
+import java.io.File
+
 fun main() {
-    println("Hello World!")
+    val workingDirectory = System.getProperty("user.dir")
+    File("$workingDirectory/Datos_Guardado/Armor_Set.txt").writeText("A", Charsets.UTF_8)
+    File("$workingDirectory/Datos_Guardado/Weapons_Set.txt").writeText("Palindrome ; Retold Tale ; Apex Predator", Charsets.UTF_8)
+    File("$workingDirectory/Datos_Guardado/Vault.txt").writeText("ASDFASDFASDF ; ADSFASDFASF ; ADFASDFA", Charsets.UTF_8)
+    val armaAleaoria = File("$workingDirectory/Loot_Pool/Trials.txt").useLines { it.toList() }.random()
+    println(armaAleaoria)
 }
